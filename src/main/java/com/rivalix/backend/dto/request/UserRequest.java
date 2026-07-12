92a4728f1e07b7765f70d3145b71d4fd7b05d126
JuanPerezT <juandavidpereztorres6@gmail.com>
2026-07-12 00:40:54 -0500

    Add UserRequest and UserResponse DTOs

---
+++ src/main/java/com/rivalix/backend/dto/request/UserRequest.java
@@ -0,0 +1,26 @@
+package com.rivalix.backend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+    
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 255)
+    private String password;
+}
